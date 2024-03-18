59f6ce387d579c94bce1f99903262bab812e69e7
Atfayee <jingchi.zhang@outlook.com>
2024-03-17 23:35:12 -0400

    core layer completed

--- core/exceptions.py
+++ core/exceptions.py
@@ -21,7 +21,20 @@ class ConstraintViolationError(ImageGenerationException):
         super().__init__(message)
 
 # 2. Service Layer Exceptions
-        
+class DataValidationError(ImageGenerationException):
+    def __init__(self, message="Provided data is invalid."):
+        super().__init__(message)
+
+
+class UnauthorizedError(ImageGenerationException):
+    def __init__(self, message="Unauthorized access."):
+        super().__init__(message)
+
+
+class OperationNotAllowedError(ImageGenerationException):
+    def __init__(self, message="This operation is not allowed."):
+        super().__init__(message)
+
 # 3. Web Layer Exceptions
 class BadRequestError(ImageGenerationException):
     def __init__(self, message="Bad request data."):
@@ -32,15 +45,19 @@ class EndpointNotFoundError(ImageGenerationException):
     def __init__(self, message="Endpoint not found."):
         super().__init__(message)
 
+class AuthenticationError(ImageGenerationException):
+    def __init__(self, message="Authentication failed."):
+        super().__init__(message)
+
 EXCEPTION_STATUS_CODES = {
-    # DataValidationError: 400,       # Bad Request
+    DataValidationError: 400,       # Bad Request
     ConstraintViolationError: 409,  # Conflict
-    ImageGenerationException: 500,           # Internal Server Error (Generic fallback)
+    ImageGenerationException: 500,  # Internal Server Error (Generic fallback)
     DBConnectionError: 500,         # Internal Server Error (Generic fallback)
     RecordNotFoundError: 404,       # Not Found
-    # UnauthorizedError: 401,         # Unauthorized
-    # OperationNotAllowedError: 403,  # Forbidden
+    UnauthorizedError: 401,         # Unauthorized
+    OperationNotAllowedError: 403,  # Forbidden
     BadRequestError: 400,           # Bad Request
     EndpointNotFoundError: 404,     # Not Found
-    # AuthenticationError: 401,       # Unauthorized
+    AuthenticationError: 401,       # Unauthorized
 }
