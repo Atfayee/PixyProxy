14f77c712a3d975bd9243ec43d77bc7a3873488c
Atfayee <jingchi.zhang@outlook.com>
2024-03-19 10:35:45 -0400

    TypeError: core.models.ImageDetail() argument after ** must be a mapping, not str

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 # Register routers
 
 # Include the router with a prefix
-app.include_router(router, prefix="/api")
+app.include_router(router, prefix="/image")
 # Innermost first
 app.add_middleware(LoggingMiddleware)  # type: ignore
 app.add_middleware(RequestIdMiddleware)  # type: ignore
