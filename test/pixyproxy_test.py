dae5f3773da00ee996de0590fa41e294a66a07ae
Atfayee <jingchi.zhang@outlook.com>
2024-03-18 22:24:58 -0400

    solve core module problem and created database and image table

--- test/pixyproxy_test.py
+++ test/pixyproxy_test.py
@@ -1,9 +1,14 @@
 import os
-
+import sys
 import httpx
 import mysql.connector
 import pytest
 from dotenv import load_dotenv
+# Get the absolute path to the project root directory
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+# Add the project root directory to the Python path
+sys.path.insert(0, project_root)
 
 from core.models import ImageDetail
 
